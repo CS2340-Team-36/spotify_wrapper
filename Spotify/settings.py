07004adfb7e7ada65f32527c7b92c0c295de1eaf
Ananya Pattamatta <ananyapattamatta@gmail.com>
2024-11-11 22:18:45 -0500

    toggle button for dark mode works

--- Spotify/settings.py
+++ Spotify/settings.py
@@ -17,6 +17,13 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_URL = '/static/'
+
+# Optional: If you have a custom static directory
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/

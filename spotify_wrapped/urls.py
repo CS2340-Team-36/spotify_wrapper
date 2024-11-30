630da57b1952409ef3d935f520cbb20c8d073e2f
Jahnavi Manglik <144987804+jahnavimanglik@users.noreply.github.com>
2024-11-30 10:14:09 -0500

    Revert "added share to socials and language toggle functionality"

--- spotify_wrapped/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),  # Add a view for the root path
-    path('set-language/', views.set_language, name='set_language'),
-    path('share/', views.share_wrapped, name='share_wrapped'),
-]
